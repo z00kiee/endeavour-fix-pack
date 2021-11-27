b721551a002411a90e1c70e92d373e3d13f8c0ed
Nick <67544305+z00kiee@users.noreply.github.com>
2021-11-27 09:05:21 -0800

    Enable Bluetooth

--- yes-blue.sh
+++ yes-blue.sh
@@ -2,7 +2,7 @@
 
 echo "This script enable Bluetooth devices for EndeavourOS without the hassle"
 
-echo "to enable Bluetooth devices TYPE: yes"
+echo "To enable Bluetooth devices TYPE: yes"
 read yes
 #Install Bluetooth
 sudo pacman -S --needed bluez bluez-utils pulseaudio-bluetooth -y $yes
