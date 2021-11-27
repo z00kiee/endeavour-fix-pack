0323d6f2801388114d7899ae0768b308bea9476c
Nick <67544305+z00kiee@users.noreply.github.com>
2021-11-27 06:43:25 -0800

    Update yes-blue.sh

--- yes-blue.sh
+++ yes-blue.sh
@@ -3,11 +3,10 @@
 #yes-blue
 echo "This script enable bluetooth devices for endeavouros without the hassle"
 
-echo "would you to enable bluetooth devices {Type y for yes]"
-read y
+echo "would you to enable bluetooth devices {Type yes]"
+read yes
 #Install Bluetooth and enable it
-sudo pacman -S --needed bluez bluez-utils pulseaudio-bluetooth -y $y
-exit
+sudo pacman -S --needed bluez bluez-utils pulseaudio-bluetooth -yes $y
 echo "Enabling Bluetooth Service"
 sudo systemctl enable --now bluetooth
 exit
